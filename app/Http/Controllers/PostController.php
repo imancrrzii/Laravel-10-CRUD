4bb9eaeb8b6eca40896dd9ed319ef72ed12a92eb
imancrrzii <imancarrazi777@gmail.com>
2023-05-29 00:08:19 +0700

    Menambahkan edit dan show

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Post;
 use Illuminate\View\View;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class PostController extends Controller
 {
@@ -35,5 +36,47 @@ class PostController extends Controller
         ]);
 
         return redirect()->route('posts.index')->with(['success' => 'Data berhasil disimpan!']);
+    }
+
+    public function show($id){
+        $post = Post::findOrFail($id);
+
+        return view('posts.show', compact('post'));
+    }
+
+    public function edit($id){
+        $post = Post::findOrFail($id);
+
+        return view('posts.edit', compact('post'));
+    }
+
+    public function update(Request $request, $id){
+        $this->validate($request, [
+            'image' => 'image|mimes:png,jpg|max:2048',
+            'title' => 'required|min:5',
+            'content' => 'required|min:10',
+        ]);
+
+        $post = Post::findOrFail($id);
+
+        if($request->hasFile('image')){
+
+            $image = $request->file('image');
+            $image->storeAs('public/posts', $image->hashName());
+
+            Storage::delete('public/posts/'.$post->image);
+
+            $post->update([
+                'image' => $image->hashName(),
+                'title' => $request->title,
+                'content' => $request->content
+            ]);
+        } else {
+            $post->update([
+                'title' => $request->title,
+                'content' => $request->content,
+            ]);
+        }
+        return redirect()->route('posts.index')->with(['success' => 'Data berhasil di update']);
     }
 }

74e2ca6de02e7d4f492729e2f469b777be0958a4
imancrrzii <imancarrazi777@gmail.com>
2023-05-28 23:48:18 +0700

    Menambahkan tambah data

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -13,4 +13,27 @@ class PostController extends Controller
         $posts = Post::latest()->paginate(5);
         return view('posts.index', compact('posts'));
     }
+
+    public function create(){
+        return view('posts.create');
+    }
+
+    public function store(Request $request){
+        $this->validate($request, [
+            'image'     => 'required|image|mimes:png,jpg,jpeg|max:2048',
+            'title'     => 'required|min:5',
+            'content'   => 'required|min:10'
+        ]);
+
+        $image = $request->file('image');
+        $image->storeAs('public/posts', $image->hashName());
+
+        Post::create([
+            'image'     => $image->hashName(),
+            'title'     => $request->title,
+            'content'   => $request->content
+        ]);
+
+        return redirect()->route('posts.index')->with(['success' => 'Data berhasil disimpan!']);
+    }
 }
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -5,36 +5,30 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Data Posts - SantriKoding.com</title>
+    <title>Data Posts</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
 </head>
 
-<body style="background: lightgray">
-
+<body>
     <div class="container mt-5">
         <div class="row">
             <div class="col-md-12">
-                <div>
-                    <h3 class="text-center my-4">Tutorial Laravel 10 untuk Pemula</h3>
-                    <h5 class="text-center"><a href="https://santrikoding.com">www.santrikoding.com</a></h5>
-                    <hr>
-                </div>
                 <div class="card border-0 shadow-sm rounded">
                     <div class="card-body">
-                        <a href="{{ route('posts.create') }}" class="btn btn-md btn-success mb-3">TAMBAH POST</a>
+                        <a href="{{ route('posts.create') }}" class="btn btn-primary mb-3">Tambah Data</a>
                         <table class="table table-bordered">
                             <thead>
-                                <tr>
-                                    <th scope="col">GAMBAR</th>
-                                    <th scope="col">JUDUL</th>
-                                    <th scope="col">CONTENT</th>
-                                    <th scope="col">AKSI</th>
+                                <tr class="text-center">
+                                    <th scope="col">Gambar</th>
+                                    <th scope="col">Judul</th>
+                                    <th scope="col">Konten</th>
+                                    <th scope="col">Aksi</th>
                                 </tr>
                             </thead>
                             <tbody>
                                 @forelse ($posts as $post)
-                                    <tr>
+                                    <tr class="text-center">
                                         <td class="text-center">
                                             <img src="{{ asset('/storage/posts/' . $post->image) }}" class="rounded"
                                                 style="width: 150px">
@@ -45,12 +39,12 @@
                                             <form onsubmit="return confirm('Apakah Anda Yakin ?');"
                                                 action="{{ route('posts.destroy', $post->id) }}" method="POST">
                                                 <a href="{{ route('posts.show', $post->id) }}"
-                                                    class="btn btn-sm btn-dark">SHOW</a>
+                                                    class="btn btn-primary">Detail</a>
                                                 <a href="{{ route('posts.edit', $post->id) }}"
-                                                    class="btn btn-sm btn-primary">EDIT</a>
+                                                    class="btn btn-warning">Edit</a>
                                                 @csrf
                                                 @method('DELETE')
-                                                <button type="submit" class="btn btn-sm btn-danger">HAPUS</button>
+                                                <button type="submit" class="btn btn-danger">Hapus</button>
                                             </form>
                                         </td>
                                     </tr>
@@ -75,14 +69,10 @@
     <script>
         //message with toastr
         @if (session()->has('success'))
-
             toastr.success('{{ session('success') }}', 'BERHASIL!');
         @elseif (session()->has('error'))
-
             toastr.error('{{ session('error') }}', 'GAGAL!');
         @endif
     </script>
-
 </body>
-
 </html>
